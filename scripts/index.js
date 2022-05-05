d318acb67ceec5e2a357228e7c52a9d2ef790152
KaustubhBadvegit config --global user.name KaustubhBadvegit config --global user.name KaustubhBadvegit config --global user.name KaustubhBadve <kaustubhbadve7@gmail.com>
2022-05-05 10:36:48 +0530

    kaustubh

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,248 @@ document.getElementById("footer").innerHTML=footer()
 
 
 
+
+document.getElementById("profilesymbol").addEventListener("click",showlogindiv)
+
+function showlogindiv(){
+  document.getElementById("profile").style.display="block"
+}
+
+
+
+
+
+document.getElementById("searchsymbol").addEventListener("click",searchProduct)
+
+
+async function searchProduct(){
+  try{
+
+let query=document.getElementById("inputsearch").value
+
+let res=await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=${query}`)
+
+let data=await res.json()
+
+console.log(data)
+showSearch(data)
+
+  }catch(err)
+  {
+    console.log(err)
+  }
+}
+
+
+function showSearch(data){
+
+  document.getElementById("searchresultdiv").innerHTML=null
+  document.getElementById("searchresultdiv").style.display="block"
+  data.forEach(function(elem){
+
+    let main=document.createElement("div")
+    main.setAttribute("id","searchmain")
+
+    let name=document.createElement("div")
+    name.innerText=elem.name
+    name.setAttribute("id","searchname")
+
+    let imagdiv=document.createElement("div")
+    imagdiv.setAttribute("id","searchimgdiv")
+
+    let image_link=document.createElement("img")
+    image_link.src=elem.image_link
+
+    imagdiv.append(image_link)
+
+    let brand=document.createElement("div")
+    brand.innerText="Brand Name : "+elem.brand
+    brand.setAttribute("id","brandname")
+    
+
+    let description=document.createElement("div")
+    description.innerText="Description"+"  "+elem.description
+    description.setAttribute("id","searchdescription")
+
+    let merge=document.createElement("div")
+    merge.append(name,brand,description)
+    merge.setAttribute("id","mergesearch")
+
+    main.append(imagdiv,merge)
+
+    document.getElementById("searchresultdiv").append(main)
+  })
+ 
+}
+
+
+
+
+document.getElementById("navbartitle1").addEventListener("mouseover",mouseover)
+
+function mouseover(){
+
+document.querySelector("#A").style.display="block"
+document.querySelector("#divbelownavbar").style.marginTop="-250px"
+}
+
+document.getElementById("navbartitle1").addEventListener("mouseout",mouseout)
+
+function mouseout(){
+
+document.querySelector("#A").style.display="none"
+document.querySelector("#divbelownavbar").style.marginTop="10px"
+}
+
+
+
+
+
+document.getElementById("navbartitle2").addEventListener("mouseover",mouseover2)
+
+function mouseover2(){
+
+document.querySelector("#BB").style.display="block"
+document.querySelector("#divbelownavbar").style.marginTop="-250px"
+}
+
+document.getElementById("navbartitle2").addEventListener("mouseout",mouseout2)
+
+function mouseout2(){
+
+document.querySelector("#BB").style.display="none"
+document.querySelector("#divbelownavbar").style.marginTop="10px"
+}
+
+
+
+document.getElementById("navbartitle3").addEventListener("mouseover",mouseover3)
+
+function mouseover3(){
+
+document.querySelector("#CC").style.display="block"
+document.querySelector("#divbelownavbar").style.marginTop="-250px"
+}
+
+document.getElementById("navbartitle3").addEventListener("mouseout",mouseout3)
+
+function mouseout3(){
+
+document.querySelector("#CC").style.display="none"
+document.querySelector("#divbelownavbar").style.marginTop="10px"
+}
+
+
+
+
+document.getElementById("navbartitle4").addEventListener("mouseover",mouseover4)
+
+function mouseover4(){
+
+document.querySelector("#DD").style.display="block"
+document.querySelector("#divbelownavbar").style.marginTop="-250px"
+}
+
+document.getElementById("navbartitle4").addEventListener("mouseout",mouseout4)
+
+function mouseout4(){
+
+document.querySelector("#DD").style.display="none"
+document.querySelector("#divbelownavbar").style.marginTop="10px"
+}
+
+
+
+
+
+
+document.getElementById("navbartitle5").addEventListener("mouseover",mouseover5)
+
+function mouseover5(){
+
+document.querySelector("#EE").style.display="block"
+document.querySelector("#divbelownavbar").style.marginTop="-250px"
+}
+
+document.getElementById("navbartitle5").addEventListener("mouseout",mouseout5)
+
+function mouseout5(){
+
+document.querySelector("#EE").style.display="none"
+document.querySelector("#divbelownavbar").style.marginTop="10px"
+}
+
+
+
+
+
+
+
+document.getElementById("navbartitle6").addEventListener("mouseover",mouseover6)
+
+function mouseover6(){
+
+document.querySelector("#AA").style.display="block"
+document.querySelector("#divbelownavbar").style.marginTop="-250px"
+}
+
+document.getElementById("navbartitle6").addEventListener("mouseout",mouseout6)
+
+function mouseout6(){
+
+document.querySelector("#AA").style.display="none"
+document.querySelector("#divbelownavbar").style.marginTop="10px"
+}
+
+
+
+ //To keep the navbar position fix
+
+window.onscroll = function() {myFunctionn()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunctionn() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // For sliding images
 
 let slideimg=["https://www.beautybebo.com/pub/media/mega-menu/slider-2.jpg","https://www.beautybebo.com/pub/media/mega-menu/slider-1.jpg","https://www.beautybebo.com/pub/media/mega-menu/slider-3.jpg"]
@@ -54,20 +296,8 @@ function myFunction() {
 
 
 
-  //To keep the navbar position fix
+ 
 
-  window.onscroll = function() {myFunctionn()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunctionn() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
 
 
 // Product constructor function
@@ -307,121 +537,7 @@ Verticalappend(16,"q")
 
 
 
-document.getElementById("navbartitle1").addEventListener("mouseover",mouseover)
 
-function mouseover(){
-
-document.querySelector("#A").style.display="block"
-document.querySelector("#divbelownavbar").style.marginTop="-250px"
-}
-
-document.getElementById("navbartitle1").addEventListener("mouseout",mouseout)
-
-function mouseout(){
-
-document.querySelector("#A").style.display="none"
-document.querySelector("#divbelownavbar").style.marginTop="10px"
-}
-
-
-
-
-
-document.getElementById("navbartitle2").addEventListener("mouseover",mouseover2)
-
-function mouseover2(){
-
-document.querySelector("#BB").style.display="block"
-document.querySelector("#divbelownavbar").style.marginTop="-250px"
-}
-
-document.getElementById("navbartitle2").addEventListener("mouseout",mouseout2)
-
-function mouseout2(){
-
-document.querySelector("#BB").style.display="none"
-document.querySelector("#divbelownavbar").style.marginTop="10px"
-}
-
-
-
-document.getElementById("navbartitle3").addEventListener("mouseover",mouseover3)
-
-function mouseover3(){
-
-document.querySelector("#CC").style.display="block"
-document.querySelector("#divbelownavbar").style.marginTop="-250px"
-}
-
-document.getElementById("navbartitle3").addEventListener("mouseout",mouseout3)
-
-function mouseout3(){
-
-document.querySelector("#CC").style.display="none"
-document.querySelector("#divbelownavbar").style.marginTop="10px"
-}
-
-
-
-
-document.getElementById("navbartitle4").addEventListener("mouseover",mouseover4)
-
-function mouseover4(){
-
-document.querySelector("#DD").style.display="block"
-document.querySelector("#divbelownavbar").style.marginTop="-250px"
-}
-
-document.getElementById("navbartitle4").addEventListener("mouseout",mouseout4)
-
-function mouseout4(){
-
-document.querySelector("#DD").style.display="none"
-document.querySelector("#divbelownavbar").style.marginTop="10px"
-}
-
-
-
-
-
-
-document.getElementById("navbartitle5").addEventListener("mouseover",mouseover5)
-
-function mouseover5(){
-
-document.querySelector("#EE").style.display="block"
-document.querySelector("#divbelownavbar").style.marginTop="-250px"
-}
-
-document.getElementById("navbartitle5").addEventListener("mouseout",mouseout5)
-
-function mouseout5(){
-
-document.querySelector("#EE").style.display="none"
-document.querySelector("#divbelownavbar").style.marginTop="10px"
-}
-
-
-
-
-
-
-
-document.getElementById("navbartitle6").addEventListener("mouseover",mouseover6)
-
-function mouseover6(){
-
-document.querySelector("#AA").style.display="block"
-document.querySelector("#divbelownavbar").style.marginTop="-250px"
-}
-
-document.getElementById("navbartitle6").addEventListener("mouseout",mouseout6)
-
-function mouseout6(){
-
-document.querySelector("#AA").style.display="none"
-document.querySelector("#divbelownavbar").style.marginTop="10px"
-}
 
 
 
@@ -507,68 +623,7 @@ document.getElementById("SideHE").style.display="none"
 }
 
 
-document.getElementById("searchsymbol").addEventListener("click",searchProduct)
 
-
-async function searchProduct(){
-  try{
-
-let query=document.getElementById("inputsearch").value
-
-let res=await fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=${query}`)
-
-let data=await res.json()
-
-console.log(data)
-showSearch(data)
-
-  }catch(err)
-  {
-    console.log(err)
-  }
-}
-
-
-function showSearch(data){
-
-  document.getElementById("searchresultdiv").innerHTML=null
-  document.getElementById("searchresultdiv").style.display="block"
-  data.forEach(function(elem){
-
-    let main=document.createElement("div")
-    main.setAttribute("id","searchmain")
-
-    let name=document.createElement("div")
-    name.innerText=elem.name
-    name.setAttribute("id","searchname")
-
-    let imagdiv=document.createElement("div")
-    imagdiv.setAttribute("id","searchimgdiv")
-
-    let image_link=document.createElement("img")
-    image_link.src=elem.image_link
-
-    imagdiv.append(image_link)
-
-    let brand=document.createElement("div")
-    brand.innerText="Brand Name : "+elem.brand
-    brand.setAttribute("id","brandname")
-    
-
-    let description=document.createElement("div")
-    description.innerText="Description"+"  "+elem.description
-    description.setAttribute("id","searchdescription")
-
-    let merge=document.createElement("div")
-    merge.append(name,brand,description)
-    merge.setAttribute("id","mergesearch")
-
-    main.append(imagdiv,merge)
-
-    document.getElementById("searchresultdiv").append(main)
-  })
- 
-}
 
 
   document.querySelector("#divbelownavbar").addEventListener("click",removesearch)
@@ -580,8 +635,12 @@ function showSearch(data){
   }
 
 
-  document.getElementById("profilesymbol").addEventListener("click",showlogindiv)
 
-  function showlogindiv(){
-    document.getElementById("profile").style.display="block"
-  }
+
+
+
+
+
+
+
+  // function 
